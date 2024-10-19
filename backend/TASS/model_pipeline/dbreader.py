15b001d167ba8d6bd8ad882ca702410fd1dde55e
Anirudh2003 <89632248+Anirudh2003@users.noreply.github.com>
2024-10-19 23:12:20 +0530

    Update dbreader.py

--- backend/TASS/model_pipeline/dbreader.py
+++ backend/TASS/model_pipeline/dbreader.py
@@ -1,38 +1,5 @@
 from pymongo import MongoClient
 
-# class DbReader:
-    
-#     MONGO_URI = 'mongodb://localhost:27017/?readPreference=primary&replicaSet=rs0'
-#     client = MongoClient(MONGO_URI)
-#     db = client["Carved_Files"]
-
-#     def run(self, socketio):
-#         print("in run")
-#         self.retrive_one_doc(socketio)
-
-#     def retrive_one_doc(self, socketio):
-#         collections = self.db.list_collection_names()
-#         for collection in collections:
-#             coll = self.db[collection]
-#             doc = coll.find_one()  # Retrieve one document
-#             if doc:  # Ensure that a document was found
-#                 file_content_binary = doc.get('file_content')
-#                 file_name = doc.get('file_name')
-#                 identity = str(doc.get('_id'))
-#                 print("type", identity)
-
-#                 # Assuming file_content_binary is already in a suitable format
-#                 image_data = f"data:image;base64,{file_content_binary}"
-
-#                 socketio.emit('image_name', {
-#                     'content': {"data": image_data,
-#                                 "id": identity},
-#                     'folder': f"{collection}"
-#                 })
-
-
-
-
 class DbReader_Ml:
     MONGO_URI = 'mongodb://localhost:27017'
     client = MongoClient(MONGO_URI)
@@ -64,7 +31,7 @@ class DbReader_Ml:
                     })
 class DbReader:
     
-    MONGO_URI = 'mongodb://localhost:27017/?readPreference=primary&replicaSet=rs0'
+    MONGO_URI = 'mongodb://localhost:27017/'
     client = MongoClient(MONGO_URI)
     db = client["Carved_Files"]
 
@@ -96,7 +63,7 @@ class DbReader:
 
 
 class DbReader_Ml_nude:
-    MONGO_URI = 'mongodb://localhost:27017/?readPreference=primary&replicaSet=rs0'
+    MONGO_URI = 'mongodb://localhost:27017/'
     client = MongoClient(MONGO_URI)
     db = client["ML_Files"]
 
@@ -129,7 +96,7 @@ class DbReader_Ml_nude:
                     
 
 class Dbreader_Face:
-    MONGO_URI = 'mongodb://localhost:27017/?readPreference=primary&replicaSet=rs0'
+    MONGO_URI = 'mongodb://localhost:27017/'
     client = MongoClient(MONGO_URI)
     db = client["Faces"]
 
