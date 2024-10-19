23c097d72a628d6e3b452d9c324be5e7e95fcf8f
Anirudh2003 <89632248+Anirudh2003@users.noreply.github.com>
2024-10-19 23:05:29 +0530

    Update predict_cars.py

--- backend/TASS/model_pipeline/predict_models/predict_cars.py
+++ backend/TASS/model_pipeline/predict_models/predict_cars.py
@@ -28,22 +28,17 @@ class Predict_Cars:
                 output = model(img)
             
                 for result in output:
-                    # print(result.boxes)
                     for box in result.boxes:
                         class_id = int(box.cls[0])
-                        # print('class id: ', class_id)
                         class_name = result.names[class_id]
                         if 'car' in class_name:
                         
                             document={'file_content': doc['file_content'],
                                         'file_name': doc['file_name']
                             }
-                            print('documetn created')
                             helpers.write_docs(key, document, self.__loop)
                             
-                            # collection.insert_one(document)
                             break
             except Exception as e:
                 print("Corrupted: ",e)
-        # helpers.write_docs(key, documents)
     
