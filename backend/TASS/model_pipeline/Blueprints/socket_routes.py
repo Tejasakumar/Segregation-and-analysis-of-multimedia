88fcc35ddab1095d98fe2974be7ae0d88a89f7af
Anirudh2003 <89632248+Anirudh2003@users.noreply.github.com>
2024-10-19 23:01:55 +0530

    Update socket_routes.py

--- backend/TASS/model_pipeline/Blueprints/socket_routes.py
+++ backend/TASS/model_pipeline/Blueprints/socket_routes.py
@@ -15,29 +15,6 @@ socketio_blueprint = Blueprint('socketio_blueprint', __name__)
 socketio  = SocketIO(cross_origin="*", async_mode='threading')
 t1 = None
 
-# MONGO_URI = 'mongodb://localhost:27017/?readPreference=primary&replicaSet=rs0'
-# client = AsyncIOMotorClient(MONGO_URI)
-# db = client["tejas_carved_files"]
-
-# @socketio.on('connect',namespace="/raw")
-# def handle_connect():
-#     print('Client connected')
-
-
-# @socketio.on('connect', namespace="/ml")
-# def handle_connect():
-#     print('ML Client connected')
-    
-
-# @socketio.on('disconnect')
-# def handle_disconnect():
-#     print('Client disconnected')
-
-# @socketio.on('Ready')
-# def handle_message():
-#     print("got ready")
-#     t1 = threading.Thread(target=helpers.gettin_preview_thumb,args=(db,socketio))
-#     t1.start()
 
 socketio.on_namespace(FetchNamespace.fetchUnclassified('/raw'))
 socketio.on_namespace(MLGeneralNamespace.ML_GeneralNamespace('/mlgen'))
